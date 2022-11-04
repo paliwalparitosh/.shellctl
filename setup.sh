9c6122820414fdb12bd3fad1f32aa464555c2ff3
paliwalparitosh <paritoshedx@gmail.com>
2022-11-04 19:15:16 +0530

    rename and refactor

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # copy vim rc to HOME
-cp -f .vimrc ~/.vimrc
+cp -f vimrc ~/.vimrc
 
 # add alias to ~/.bashrc
 cat ~/.bashrc | egrep my-bash-setup >/dev/null && v_ret=$? || v_ret=$?
 
 if [ $v_ret -ne 0 ]; then
     echo "#Added by my-bash-setup" >> ~/.bashrc
-    echo "source ~/my-bash-setup/alias.sh" >> ~/.bashrc
+    echo "source ~/my-bash-setup/bashrc" >> ~/.bashrc
 fi
 
 echo "Setup complete. run -> bash"
